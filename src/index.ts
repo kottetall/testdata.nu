948fcbd6064bc691b59dc5d73301e695e176d3dd
Alexander Hedling <taikes_bass@hotmail.com>
2026-04-23 10:50:16 +0200

    fix: exposed the new method getOrganizationName

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./generators/getSsn.js";
 export * from "./generators/getName.js";
 export * from "./generators/getPhonenumber.js";
 export * from "./generators/getProfession.js";
+export * from "./generators/getOrganizationName.js";
